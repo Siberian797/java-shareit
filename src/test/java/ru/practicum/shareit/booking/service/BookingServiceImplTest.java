f79dc4c82732bb342ae3748292b27208238ed13e
vv <alfervladimir@yandex.ru>
2023-06-13 19:47:23 +0300

    test: бранч покрытие 6

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -1370,6 +1370,148 @@ class BookingServiceImplTest {
     }
 
     /**
+     * Method under test: {@link BookingServiceImpl#updateBooking(long, boolean, long)}
+     */
+    @Test
+    void testUpdateBooking7() {
+        when(bookingMapper.toResponseDto(Mockito.any(), Mockito.any(), Mockito.any()))
+                .thenReturn(new BookingResponseDto());
+
+        User booker = new User();
+        booker.setEmail("jane.doe@example.org");
+        booker.setId(10L);
+        booker.setName("Name");
+
+        User owner = new User();
+        owner.setEmail("jane.doe@example.org");
+        owner.setId(1L);
+        owner.setName("Name");
+
+        User requester = new User();
+        requester.setEmail("jane.doe@example.org");
+        requester.setId(1L);
+        requester.setName("Name");
+
+        Request request = new Request();
+        request.setCreatedTime(LocalDate.of(1970, 1, 1).atStartOfDay());
+        request.setDescription("The characteristics of someone or something");
+        request.setId(1L);
+        request.setRequester(requester);
+
+        Item item = new Item();
+        item.setAvailable(true);
+        item.setDescription("The characteristics of someone or something");
+        item.setId(1L);
+        item.setName("Name");
+        item.setOwner(owner);
+        item.setRequest(request);
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+        doNothing().when(user).setEmail(Mockito.any());
+        doNothing().when(user).setId(Mockito.<Long>any());
+        doNothing().when(user).setName(Mockito.any());
+        user.setEmail("jane.doe@example.org");
+        user.setId(1L);
+        user.setName("Name");
+
+        User owner2 = new User();
+        owner2.setEmail("jane.doe@example.org");
+        owner2.setId(1L);
+        owner2.setName("Name");
+
+        User requester2 = new User();
+        requester2.setEmail("jane.doe@example.org");
+        requester2.setId(1L);
+        requester2.setName("Name");
+
+        Request request2 = new Request();
+        request2.setCreatedTime(LocalDate.of(1970, 1, 1).atStartOfDay());
+        request2.setDescription("The characteristics of someone or something");
+        request2.setId(1L);
+        request2.setRequester(requester2);
+
+        Item item2 = new Item();
+        item2.setAvailable(true);
+        item2.setDescription("The characteristics of someone or something");
+        item2.setId(1L);
+        item2.setName("Name");
+        item2.setOwner(owner2);
+        item2.setRequest(request2);
+        Booking booking = mock(Booking.class);
+        when(booking.getItem()).thenReturn(item2);
+        when(booking.getBooker()).thenReturn(user);
+        when(booking.getStatus()).thenReturn(BookingStatus.WAITING);
+
+        booking.setBooker(booker);
+        booking.setEnd(LocalDate.of(1970, 1, 1).atStartOfDay());
+        booking.setId(1L);
+        booking.setItem(item);
+        booking.setStart(LocalDate.of(1970, 1, 1).atStartOfDay());
+        booking.setStatus(BookingStatus.WAITING);
+
+        User booker2 = new User();
+        booker2.setEmail("jane.doe@example.org");
+        booker2.setId(1L);
+        booker2.setName("Name");
+
+        User owner3 = new User();
+        owner3.setEmail("jane.doe@example.org");
+        owner3.setId(1L);
+        owner3.setName("Name");
+
+        User requester3 = new User();
+        requester3.setEmail("jane.doe@example.org");
+        requester3.setId(1L);
+        requester3.setName("Name");
+
+        Request request3 = new Request();
+        request3.setCreatedTime(LocalDate.of(1970, 1, 1).atStartOfDay());
+        request3.setDescription("The characteristics of someone or something");
+        request3.setId(1L);
+        request3.setRequester(requester3);
+
+        Item item3 = new Item();
+        item3.setAvailable(true);
+        item3.setDescription("The characteristics of someone or something");
+        item3.setId(1L);
+        item3.setName("Name");
+        item3.setOwner(owner3);
+        item3.setRequest(request3);
+
+        Booking booking2 = new Booking();
+        booking2.setBooker(booker2);
+        booking2.setEnd(LocalDate.of(1970, 1, 1).atStartOfDay());
+        booking2.setId(1L);
+        booking2.setItem(item3);
+        booking2.setStart(LocalDate.of(1970, 1, 1).atStartOfDay());
+        booking2.setStatus(BookingStatus.WAITING);
+        when(bookingRepository.save(Mockito.any())).thenReturn(booking2);
+        when(bookingRepository.findById(Mockito.<Long>any())).thenReturn(Optional.of(booking));
+
+        User user2 = new User();
+        user2.setEmail("jane.doe@example.org");
+        user2.setId(5L);
+        user2.setName("Name");
+        Optional<User> ofResult2 = Optional.of(user2);
+        when(userRepository.findById(Mockito.<Long>any())).thenReturn(ofResult2);
+        assertThrows(EntityNotValidException.class, () -> bookingServiceImpl.updateBooking(1L, true, 5L));
+        verify(bookingRepository).findById(Mockito.<Long>any());
+        verify(booking).getStatus();
+        verify(booking).getBooker();
+        verify(booking).setBooker(Mockito.any());
+        verify(booking).setEnd(Mockito.any());
+        verify(booking).setId(Mockito.<Long>any());
+        verify(booking).setItem(Mockito.any());
+        verify(booking).setStart(Mockito.any());
+        verify(booking).setStatus(Mockito.any());
+        verify(user).getId();
+        verify(user).setEmail(Mockito.any());
+        verify(user).setId(Mockito.<Long>any());
+        verify(user).setName(Mockito.any());
+        verify(userRepository).findById(Mockito.<Long>any());
+    }
+
+    /**
      * Method under test: {@link BookingServiceImpl#getBookings(BookingState, long, PageRequest)}
      */
     @Test
